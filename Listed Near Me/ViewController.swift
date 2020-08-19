d19b9034387fe32751f40f7b81168dfeeaf79dec
Stuart Baker <stuart.baker1@sainsburys.co.uk>
2020-08-19 20:02:09 +0100

    Initial Commit

---
+++ Listed Near Me/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Listed Near Me
+//
+//  Created by Stuart Baker on 19/08/2020.
+//  Copyright © 2020 Stuart Baker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Listed Near MeTests/Listed_Near_MeTests.swift
@@ -0,0 +1,34 @@
+//
+//  Listed_Near_MeTests.swift
+//  Listed Near MeTests
+//
+//  Created by Stuart Baker on 19/08/2020.
+//  Copyright © 2020 Stuart Baker. All rights reserved.
+//
+
+import XCTest
+@testable import Listed_Near_Me
+
+class Listed_Near_MeTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
