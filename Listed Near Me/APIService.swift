fbf02064c92fcff98768ec5e04bf0793cb6a5c6b
Stuart Baker <stuart.baker1@sainsburys.co.uk>
2020-09-07 09:35:38 +0100

    Add method to get nearby listings

--- Listed Near Me/APIService.swift
+++ Listed Near Me/APIService.swift
@@ -37,6 +37,33 @@ class APIService {
         }.resume()
     }
     
+    func getNear(_ location: Location, completion: @escaping (Result<[Listing], Error>) -> Void) {
+        var url = createURLComponents(for: "near")
+        url.queryItems = [
+            URLQueryItem(name: "long", value: String(location.long)),
+            URLQueryItem(name: "lat", value: String(location.lat))
+        ]
+        
+        guard let validUrl = url.url else {
+            return // TODO: Error handling
+        }
+        
+        URLSession.shared.dataTask(with: validUrl) { (data, response, error) in
+            // TOOD: reduce duplication
+            guard let validData = data, error == nil else {
+                 completion(.failure(error!))
+                 return
+             }
+             
+            do {
+                 let listings = try JSONDecoder().decode([Listing].self, from: validData)
+                 completion(.success(listings))
+             } catch let serializationError {
+                 completion(.failure(serializationError))
+             }
+        }.resume()
+    }
+    
     private func createURLComponents(for path: String) -> URLComponents {
         var url = URLComponents()
         url.scheme = scheme
--- Listed Near Me/ViewController.swift
+++ Listed Near Me/ViewController.swift
@@ -7,12 +7,38 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController, UITableViewDataSource {
+class ViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     
     private var listings: [Listing] = []
+    private let api = APIService()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func refreshLocation(_ sender: Any) {
+        let currentLocation = Location(long: 0, lat: 0)
+        self.api.getNear(currentLocation) { (result) in
+            switch result {
+                case .success(let listings):
+                    self.listings = listings
+                    DispatchQueue.main.async {
+                        self.tableView.reloadData()
+                    }
+                case .failure(let error):
+                    print(error) // TODO: Better error handling
+            }
+        }
+    }
+    
+}
+
+extension ViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.listings.count
@@ -29,21 +55,4 @@ class ViewController: UIViewController, UITableViewDataSource {
         return cell
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        let api = APIService()
-        api.getAll() { (result) in
-            switch result {
-                case .success(let listings):
-                    self.listings = listings
-                    DispatchQueue.main.async {
-                        self.tableView.reloadData()
-                    }
-                case .failure(let error):
-                    print(error) // TODO: Better error handling
-            }
-        }
-    }
-
 }
-
