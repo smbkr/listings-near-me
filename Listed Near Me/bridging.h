4d3505e7faeee3cea6bc4b09acb4abcee2796ba3
Stuart Baker <stuart.baker1@sainsburys.co.uk>
2020-09-27 10:41:42 +0100

    Add libspatialite

---
+++ Listed Near Me/bridging.h
@@ -0,0 +1,11 @@
+//
+//  bridging.h
+//  Listed Near Me
+//
+//  Created by Stuart Baker on 27/09/2020.
+//  Copyright © 2020 Stuart Baker. All rights reserved.
+//
+
+#include <sqlite3.h>
+#include <spatialite/gaiageo.h>
+#include <spatialite.h>
