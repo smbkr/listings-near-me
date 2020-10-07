62c9aca3c204ad3964df02a15f31659828e8bbea
Stuart Baker <stuart.baker1@sainsburys.co.uk>
2020-10-07 18:31:25 +0100

    Make location error more helpful

--- Listed Near Me/Views/ListingResultsViewController.swift
+++ Listed Near Me/Views/ListingResultsViewController.swift
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d5c67c97f9d0d90c8c61f6359d52e982117f74360aa7223fd0b6319bc6999852
-size 6234
+oid sha256:6395022cf6d925f9c0a39ea7a4b3393d7fe5d97ad5bba8d02cd0d47ac70f6470
+size 6256
