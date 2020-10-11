b5c706b7b9e130f4141ec2b34c76f6f3f001deb0
Stuart Baker <stuart.baker1@sainsburys.co.uk>
2020-10-11 14:53:15 +0100

    Add floating panel package

---
+++ Pods/Target Support Files/Pods-Listed Near Me-Listed Near MeUITests/Pods-Listed Near Me-Listed Near MeUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Listed_Near_Me_Listed_Near_MeUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Listed_Near_Me_Listed_Near_MeUITests
+@end
